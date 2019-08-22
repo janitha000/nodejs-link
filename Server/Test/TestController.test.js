247c26c66e8f1c5c206e6642ddde27461fe51fe2
janitha000 <JanithaT@99x.lk>
2019-08-22 09:04:23 +0530

    Added integration tests

---
+++ Server/Test/TestController.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const server = require('../server');
+const expect = require('chai').expect;
+
+describe('#Testcontroller testing', function()  {
+    it('respond with Hello from Janitha', function(done)  {
+        request(server).get('/test/')    
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                
+            })
+            done();
+    })
+
+    it('respond with correct compute', (done)=> {
+        request(server).get('/test/compute')
+            .expect(200)
+            .end((err, res) => {
+                expect(res.body).to.be.equal(499500);
+                done();
+            })
+    })
+})
+
+
--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const port = 3000;
 
 const app = require('./app');
@@ -6,3 +5,5 @@ const app = require('./app');
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
 })
+
+module.exports = app;
